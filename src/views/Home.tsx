cbdcd8a65109e3916d87a59960e9171acf7bc8f7
FraterSKS <jackson.a.furr@gmail.com>
2022-12-23 17:57:23 -0500

    scroll buttons functionality

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,12 +7,30 @@ const Home: React.FC = () => {
       <div className={styles.stars1}></div>
       <div className={styles.stars2}></div>
       <div className={styles.stars3}></div>
-      <div className={styles.title}>
-        <span>Jackson Furr</span>
-        <br />
-        <span>FullStack Developer</span>
+      <div className={styles.box1} id="box1">
+        <div className={styles.title}>
+          <span>Jackson Furr</span>
+          <br />
+          <span>FullStack Developer</span>
+        </div>
+        <div className={styles.scrollDownBox1}>
+          <a href="#box2">
+            <i className="fa-solid fa-arrow-down" />
+          </a>
+        </div>
       </div>
-      <div className={styles.scrollBox}><i className="fa-solid fa-arrow-down" /></div>
+      <div className={styles.box2} id="box2">
+        <div className={styles.scrollUpBox2}>
+          <a href="#box1">
+            <i className="fa-solid fa-arrow-up" />
+          </a>
+        </div>
+        <div className={styles.scrollDownBox2}>
+          <a href="#">
+            <i className="fa-solid fa-arrow-down" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

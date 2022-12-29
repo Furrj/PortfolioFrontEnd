472f42828b52d1cab5a857b30e9c5bbbcf1de3d5
FraterSKS <jackson.a.furr@gmail.com>
2022-12-28 22:00:06 -0500

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" />
       </Routes>
     </BrowserRouter>
   );

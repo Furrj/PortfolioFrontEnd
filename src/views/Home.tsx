ddef9fa8c22f00753e1059cdc08bfbeede74fb6d
FraterSKS <jackson.a.furr@gmail.com>
2022-12-29 20:52:21 -0500

    added box and renamed others

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,20 +7,32 @@ const Home: React.FC = () => {
       <div className={styles.stars1}></div>
       <div className={styles.stars2}></div>
       <div className={styles.stars3}></div>
-      <div className={styles.box1} id="box1">
+      <div className={styles.box1} id="landing">
         <div className={styles.title}>
           <span>Jackson Furr</span>
           <br />
           <span>FullStack Developer</span>
         </div>
-        <a href="#box2" className={styles.scrollDownBox1}>
+        <a href="#about" className={styles.scrollDownBox1}>
           <div>
             <i className="fa-solid fa-arrow-down" />
           </div>
         </a>
       </div>
-      <div className={styles.box2} id="box2">
-        <a href="#box1" className={styles.scrollUpBox2}>
+      <div className={styles.box2} id="about">
+        <a href="#landing" className={styles.scrollUpBox2}>
+          <div>
+            <i className="fa-solid fa-arrow-up" />
+          </div>
+        </a>
+        <a href="#skills" className={styles.scrollDownBox2}>
+          <div>
+            <i className="fa-solid fa-arrow-down" />
+          </div>
+        </a>
+      </div>
+      <div className={styles.box3} id="skills">
+        <a href="#about" className={styles.scrollUpBox2}>
           <div>
             <i className="fa-solid fa-arrow-up" />
           </div>

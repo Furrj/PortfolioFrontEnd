4ce2b338835a2ebe20ed761a9635a8ecf10cb6a8
FraterSKS <jackson.a.furr@gmail.com>
2023-01-07 17:35:43 -0500

    taskr screenshot

--- src/comps/Taskr.tsx
+++ src/comps/Taskr.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./Taskr.module.css";
 
+//IMAGE
+import screenshot from "../images/TaskrSS.png";
+
 const Taskr = () => {
   return (
     <div className={styles.mainCont}>
       <div className={styles.pictureBox}>
-        <div className={styles.picture}></div>
+        <div className={styles.picture}>
+          <img className={styles.screenshot} src={screenshot} alt="" />
+        </div>
         <div className={styles.titleBox}>
           <h1 className={styles.title}>Taskr</h1>
           Productivity App
+          <br />
+          <a href="https://taskr-zerun.ondigitalocean.app">
+            taskr-zerun.ondigitalocean.app/mytodos
+          </a>
         </div>
       </div>
       <div className={styles.descripBox}></div>

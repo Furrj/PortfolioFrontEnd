ea646e8c1c755961cdff5c338326cc29bfb0a9f2
FraterSKS <jackson.a.furr@gmail.com>
2023-01-07 17:27:54 -0500

    chatter screenshot

--- src/comps/Chatter.tsx
+++ src/comps/Chatter.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./Chatter.module.css";
 
+//IMAGE
+import screenshot from "../images/ChatterSS.png";
+
 const Chatter = () => {
   return (
     <div className={styles.mainCont}>
       <div className={styles.pictureBox}>
-        <div className={styles.picture}></div>
+        <div className={styles.picture}>
+          <img className={styles.screenshot} src={screenshot} alt="" />
+        </div>
         <div className={styles.titleBox}>
           <h1 className={styles.title}>Chatter</h1>
           Social Media App
+          <br />
+          <a href="https://chatter-gyb5f.ondigitalocean.app/">
+            chatter-gyb5f.ondigitalocean.app
+          </a>
         </div>
       </div>
       <div className={styles.descripBox}></div>
